39f7d575d617a95b82e8121f28e83402358df6a1
NGSpace <71827944+NGSpace@users.noreply.github.com>
2026-03-24 23:10:25 +0200

    Update to 26.1

--- src/main/java/dev/ngspace/neotooltipfix/mixin/FixToolTipMixin.java
+++ src/main/java/dev/ngspace/neotooltipfix/mixin/FixToolTipMixin.java
@@ -1,5 +1,6 @@
 package dev.ngspace.neotooltipfix.mixin;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,28 +13,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import dev.ngspace.neotooltipfix.Helper;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipPositioner;
 import net.minecraft.resources.Identifier;
 
-@Mixin(GuiGraphics.class)
+@Mixin(GuiGraphicsExtractor.class)
 public abstract class FixToolTipMixin {
 
     @Shadow
     public abstract int guiWidth();
 
-    @ModifyVariable(method = "renderTooltip", at = @At(value = "HEAD"), index = 2, argsOnly = true)
+    @ModifyVariable(method = "tooltip", at = @At(value = "HEAD"), index = 2, argsOnly = true)
     public List<ClientTooltipComponent> makeListMutable(List<ClientTooltipComponent> value) {
         return new ArrayList<>(value);
     }
 
-    @Inject(method = "renderTooltip", at = @At(value = "HEAD"))
+    @Inject(method = "tooltip", at = @At(value = "HEAD"))
     public void fix(Font textRenderer, List<ClientTooltipComponent> components, int x, int y, ClientTooltipPositioner positioner, Identifier id, CallbackInfo ci) {
         Helper.newFix(components, textRenderer, x, guiWidth());
     }
 
-    @ModifyVariable(method = "renderTooltip", at = @At(value = "INVOKE", target = "Lorg/joml/Matrix3x2fStack;pushMatrix()Lorg/joml/Matrix3x2fStack;"), index = 12)
+    @ModifyVariable(method = "tooltip", at = @At(value = "INVOKE", target = "Lorg/joml/Matrix3x2fStack;pushMatrix()Lorg/joml/Matrix3x2fStack;"), index = 12)
     public int modifyRenderX(int value, Font textRenderer, List<ClientTooltipComponent> components, int x) {
         return Helper.shouldFlip(components, textRenderer, x);
     }
